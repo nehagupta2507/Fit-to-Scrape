ac50a71d828cb3245d3bd12f12fa3e0212ed2c03
Neha Gupta <er.nehaa.gupta@gmail.com>
2019-09-28 21:18:38 -0400

    Models setup

--- server.js
+++ server.js
@@ -49,6 +49,7 @@ app.get("/scrape", function(req, res) {
       // Add the text and href of every link, and save them as properties of the result object
       result.title = $(this).children("a").text();
       result.link = $(this).children("a").attr("href");
+      console.log(result)
 
       // Create a new Article using the `result` object built from scraping
       db.Article.create(result)
