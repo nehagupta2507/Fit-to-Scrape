7c192992561368291824b44ae78faf85f9197035
Neha Gupta <er.nehaa.gupta@gmail.com>
2019-09-28 22:55:40 -0400

    buzzfeed trending news scrape

--- server.js
+++ server.js
@@ -34,42 +34,11 @@ mongoose.connect("mongodb://localhost/HeadlinesGenerator", { useNewUrlParser: tr
 
 // Routes
 
-// A GET route for scraping the echoJS website
-// app.get("/scrape", function(req, res) {
-//   // First, we grab the body of the html with axios
-//   axios.get("http://www.echojs.com/").then(function(response) {
-//     // Then, we load that into cheerio and save it to $ for a shorthand selector
-//     var $ = cheerio.load(response.data);
-
-//     // Now, we grab every h2 within an article tag, and do the following:
-//     $("article h2").each(function(i, element) {
-//       // Save an empty result object
-//       var result = {};
-
-//       // Add the text and href of every link, and save them as properties of the result object
-//       result.title = $(this).children("a").text();
-//       result.link = $(this).children("a").attr("href");
-//       //console.log(result)
-
-//       // Create a new Article using the `result` object built from scraping
-//       db.Article.create(result)
-//         .then(function(dbArticle) {
-//           // View the added result in the console
-//           console.log(dbArticle);
-//         })
-//         .catch(function(err) {
-//           // If an error occurred, log it
-//           console.log(err);
-//         });
-//     });
-//     res.render("index");
-//   });
-// });
-
 app.get('/', function(req, res) {
     res.render('index');
 });
 
+//reddit Scrape
 app.get("/scrape", function(req, res) {
   // First, we grab the body of the html with axios
   axios.get("https://old.reddit.com/r/news/").then(function(response) {
@@ -101,30 +70,41 @@ app.get("/scrape", function(req, res) {
   });
 });
 
+//Tech news
 app.get("/scrape2", function(req, res) {
-  // First, we grab the body of the html with axios
   axios.get("https://www.cnet.com/news/").then(function(response) {
-    // Then, we load that into cheerio and save it to $ for a shorthand selector
     var $ = cheerio.load(response.data);
-
-    // Now, we grab every h2 within an article tag, and do the following: 
     $("h5").each(function(i, element) {
-      // Save an empty result object
       var result = {};
       console.log(result);
-      // Add the text and href of every link, and save them as properties of the result object
       result.title = $(this).children("a").text();
       result.link = $(this).children("a").attr("href");
-      //console.log(result)
-
-      // Create a new Article using the `result` object built from scraping
       db.Article.create(result)
         .then(function(dbArticle) {
-          // View the added result in the console
           console.log(dbArticle);
         })
         .catch(function(err) {
-          // If an error occurred, log it
+          console.log(err);
+        });
+    });
+    res.render("index");
+  });
+}); 
+
+//Trending now route
+app.get("/scrape3", function(req, res) {
+  axios.get("https://www.buzzfeed.com/news").then(function(response) {
+    var $ = cheerio.load(response.data);
+    $("h2").each(function(i, element) {
+      var result = {};
+      console.log(result);
+      result.title = $(this).children("a").text();
+      result.link = $(this).children("a").attr("href");
+      db.Article.create(result)
+        .then(function(dbArticle) {
+          console.log(dbArticle);
+        })
+        .catch(function(err) {
           console.log(err);
         });
     });
@@ -135,13 +115,11 @@ app.get("/scrape2", function(req, res) {
 
 // Route for getting all Articles from the db
 app.get("/articles", function(req, res) {
-  // TODO: Finish the route so it grabs all of the articles
   db.Article.find({})
     .then(function(dbArticle){
         res.json(dbArticle);
     })
     .catch(function(err) {
-      // If an error occurs, send the error back to the client
       res.json(err);
     });
 });
@@ -167,8 +145,6 @@ app.get("/articles/:id", function(req, res) {
     .then(function(dbArticle){
       res.json(dbArticle);
     })
-  // and run the populate method with "note",
-  // then responds with the article with the note included
 });
 
 // Route for saving/updating an Article's associated Note
